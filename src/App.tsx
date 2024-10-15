33417dabe3967d6363141ed2fdfd41aa3e4509e8
MonikaS5 <monika.bhagade@gmail.com>
2024-10-15 12:07:45 +0100

    i bootstrap and create table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 // import { useState } from 'react'
 
 import "./App.css";
+import ExpenseList from "./expense-tracker/ExpenseList";
 
 function App() {
   // const [count, setCount] = useState(0)
 
-  return <></>;
+  return (
+    <>
+      <ExpenseList />
+    </>
+  );
 }
 
 export default App;
---
+++ src/expense-tracker/ExpenseList.tsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+const ExpenseList = () => {
+  return (
+    <div>
+      <table className="table-bordered">
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+        <tfoot></tfoot>
+      </table>
+    </div>
+  );
+};
+
+export default ExpenseList;
