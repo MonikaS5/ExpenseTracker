5ab969e79dda734afda1a8f2b2c0529b74d45bec
MonikaS5 <monika.bhagade@gmail.com>
2024-10-15 17:22:12 +0100

    components renamed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 import "./App.css";
-import ExpenseList from "./expense-tracker/ExpenseList";
-import ExpenseFilter from "./expense-tracker/ExpenseFilter";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [expense, setExpense] = useState([
-    { id: 1, description: "a", amount: 10, category: "Groceries" },
-    { id: 2, description: "a2", amount: 10, category: "Utilities" },
-    { id: 3, description: "a3", amount: 10, category: "Groceries" },
-    { id: 4, description: "a4", amount: 10, category: "Entertainment" },
+    { id: 1, description: "Apples", amount: 10, category: "Groceries" },
+    { id: 2, description: "Books", amount: 20, category: "Utilities" },
+    { id: 3, description: "Celery", amount: 10, category: "Groceries" },
+    { id: 4, description: "AirPods", amount: 50, category: "Entertainment" },
   ]);
 
   const visibleExpenses = selectedCategory
---
+++ src/expense-tracker/components/ExpenseForm.tsx
@@ -0,0 +1,9 @@
+const ExpenseForm = () => {
+  return (
+    <div>
+      <form></form>
+    </div>
+  );
+};
+
+export default ExpenseForm;
