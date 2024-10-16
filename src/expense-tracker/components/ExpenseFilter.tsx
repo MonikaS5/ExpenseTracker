dea1e0e4a8fffe0bb368fe72ddb269cc03601262
MonikaS5 <monika.bhagade@gmail.com>
2024-10-16 14:40:14 +0100

    resetCategories

--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import categories from "../categories";
 
 interface Props {
@@ -5,13 +6,27 @@ interface Props {
 }
 
 const ExpenseFilter = ({ onSelectedCategory }: Props) => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleCategoryChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const category = event.target.value;
+    setSelectedCategory(category);
+    onSelectedCategory(category);
+  };
+
+  const handleResetCategories = () => {
+    setSelectedCategory("");
+    onSelectedCategory("");
+  };
   return (
     <div>
       <select
         name="expenseFilter"
-        id=""
+        value={selectedCategory}
         className="form-control"
-        onChange={(event) => onSelectedCategory(event.target.value)}
+        onChange={handleCategoryChange}
       >
         <option value="">All Categories</option>
         {categories.map((category) => (
@@ -23,6 +38,12 @@ const ExpenseFilter = ({ onSelectedCategory }: Props) => {
         <option value="Utilities">Utilities</option>
         <option value="Entertainment">Entertainment</option> */}
       </select>
+      <button
+        className="btn btn-secondary mt-3"
+        onClick={handleResetCategories}
+      >
+        Reset Categories
+      </button>
     </div>
   );
 };
