0fa03648817f605f1e0cebf42a09f2ab96fb4c5a
MonikaS5 <monika.bhagade@gmail.com>
2024-10-16 09:56:07 +0100

    error handling

--- src/expense-tracker/components/ExpenseForm.tsx
+++ src/expense-tracker/components/ExpenseForm.tsx
@@ -12,6 +12,13 @@ const Schema = z.object({
 type ExpenseFormData = z.infer<typeof Schema>;
 
 const ExpenseForm = () => {
+  // useForm
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ExpenseFormData>({ resolver: zodResolver(Schema) });
+
   return (
     <div>
       <form>
@@ -19,19 +26,39 @@ const ExpenseForm = () => {
           <label htmlFor="description" className="form-label">
             Description
           </label>
-          <input id="description" type="text" className="form-control" />
+          <input
+            {...register("description")}
+            id="description"
+            type="text"
+            className="form-control"
+          />
+          {errors.description && (
+            <p className="text-danger">{errors.description.message}</p>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="amount" className="form-label">
             Amount
           </label>
-          <input id="amount" type="number" className="form-control" />
+          <input
+            {...register("amount")}
+            id="amount"
+            type="number"
+            className="form-control"
+          />
+          {errors.amount && (
+            <p className="text-danger">{errors.amount.message}</p>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="category" className="form-label">
             Category
           </label>
-          <select id="category" className="form-select">
+          <select
+            {...register("category")}
+            id="category"
+            className="form-select"
+          >
             <option value=""></option>
             {categories.map((category) => (
               <option key={category} value={category}>
@@ -39,6 +66,9 @@ const ExpenseForm = () => {
               </option>
             ))}
           </select>
+          {errors.category && (
+            <p className="text-danger">{errors.category.message}</p>
+          )}
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
